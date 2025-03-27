6ae85e6b5dee12e4f5232d7766aaede373039277
sondereggern@bzz.ch <sondereggern@bzz.ch>
2025-03-27 16:15:58 +0100

    Css h3 spacer klasse hinzugefügt kategorie "Grosse Aufgaben" Hinzugefügt Foldout "weitere Aufgaben" hinzugefügt. -> man beachte die seperaten object-lists für leichte übungen, schwere übungen, grosse aufgaben und weitere aufgaben in uebungen.js.  in script js (inhalt der Aufgaben) gelten keine spezielen regeln. alles im const "aufgaben"
    Todos:
    hinzufügen des aufgabeninhalts

--- frontend/js/uebungen.js
+++ frontend/js/uebungen.js
@@ -8,7 +8,18 @@ document.addEventListener("DOMContentLoaded", function() {
         { id: 3, name: "Kommas in Nebensätzen" },
         { id: 4, name: "Komplexe Satzstrukturen" },
         { id: 5, name: "Lange Aufgabe mit mehreren Kommas" },
-        { id: 6, name: "Lange Aufgabe mit mehreren Kommas 2" }// Neue große Aufgabe
+    ];
+
+    const training = [
+        { id: 6, name: "Text1" },
+        { id: 7, name: "Text2" },
+        { id: 8, name: "Text3" },
+    ];
+
+    const weitere = [
+        { id: 9, name: "Text4" },
+        { id: 10, name: "Text5" },
+        { id: 11, name: "Text6" },
     ];
 
     function erstelleButtons(liste, containerId) {
@@ -26,4 +37,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     erstelleButtons(leicht, "leicht-list");
     erstelleButtons(schwer, "schwer-list");
+    erstelleButtons(training, "training-list");
+    erstelleButtons(weitere, "weitere-list");
 });
